db0179dacda81481c43492d1b6b93e18ac1de3b9
Julian Kahlert <julian.kahlert@avionic-design.de>
2022-01-06 17:11:09 +0100

    bin: Add parser to parse source dir lists
    Signed-off-by: Julian Kahlert <julian.kahlert@avionic-design.de>

---
+++ bin/index_parser.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+BASE_PATH=$(realpath ${1})
+
+print_path_lines() {
+	cat ${1} | while read -r path
+	do
+		echo $(realpath ${BASE_PATH}/${path})
+	done
+}
+
+for file in ${BASE_PATH}/*.list
+do
+	print_path_lines "${file}"
+done
