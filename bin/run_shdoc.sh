46edc867967d0db384fb8f30ad14be7bc39dbc40
Julian Kahlert <julian.kahlert@avionic-design.de>
2022-01-07 11:21:43 +0100

    bin/run_shdoc: Support single files
    Signed-off-by: Julian Kahlert <julian.kahlert@avionic-design.de>

--- bin/run_shdoc.sh
+++ bin/run_shdoc.sh
@@ -8,4 +8,15 @@ BASE_PATH=$(realpath $(dirname ${0}))
 SHDOC_PATH="${BASE_PATH}/../doc/shared/shdoc"
 mkdir -p  "${SHDOC_PATH}"
 
-find  ${1}/ -name "*.sh" -exec ${BASE_PATH}/shdoc/shdoc {} >> ${SHDOC_PATH}/${2} \;
+if [ -d "${1}" ]
+then
+	find  ${1}/ -name "*.sh" \
+	            -exec ${BASE_PATH}/shdoc/shdoc {} >> ${SHDOC_PATH}/${2} \;
+	exit 0
+fi
+
+if [ -f "${1}" ]
+then
+	${BASE_PATH}/shdoc/shdoc ${1} >> ${SHDOC_PATH}/${2}
+	exit 0
+fi
